36ae954e47594cb68ad8a00776aa3cf9573fe695
joohyun333 <joohyun333@naver.com>
2024-02-19 03:42:45 +0900

    [F/E] [update] 아이콘 클릭시 text-overflow 조정

--- windows-front/src/components/desktop/programs/Program.vue
+++ windows-front/src/components/desktop/programs/Program.vue
@@ -1,7 +1,7 @@
 <template>
   <div :id="program_info.id" class="fixed program group select-none border m-6 flex items-center justify-start flex-col text-center border-transparent hover:border-teal-400 hover:bg-sky-400/70 w-16 h-fit left-0 " :class="'top-' + position*24" >
     <img draggable="false"  class="h-10 select-none pointer-events-none" :src='program_info.path' :alt='program_info.alt'>
-    <p :class="{'h-fit':clickedProgram===program_info.id}" class="text-xs select-none pointer-events-none text-white group-hover:h-fit text-shadow h-4  w-full px-[1px] ">{{program_info.name}}</p>
+    <p class="text-xs select-none pointer-events-none text-white group-hover:h-fit text-shadow h-4  w-full px-[1px] " :class="{'h-fit':clickedProgram===program_info.id}" >{{program_info.name}}</p>
   </div>
 </template>
 
@@ -16,8 +16,8 @@ defineProps(['program_info','position','clickedProgram'])
   white-space: nowrap;
 }
 .program:hover p {
-  text-overflow: initial !important;
   overflow: visible !important;
+  text-overflow: initial !important;
   white-space: normal !important;
 }
 </style>
